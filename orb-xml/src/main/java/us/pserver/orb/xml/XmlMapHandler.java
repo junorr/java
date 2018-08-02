197298deeb5a64ce3854df443442ba1b62bd3576
Juno Roesler <juno.rr@gmail.com>
2018-08-02 10:30:53 -0300

    one

--- orb-xml/src/main/java/us/pserver/orb/xml/XmlMapHandler.java
+++ orb-xml/src/main/java/us/pserver/orb/xml/XmlMapHandler.java
@@ -22,11 +22,10 @@
 package us.pserver.orb.xml;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.function.Predicate;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 import us.pserver.tools.misc.Tuple;
@@ -37,6 +36,11 @@ import us.pserver.tools.misc.Tuple;
  * @version 0.0 - 30/07/2018
  */
 public class XmlMapHandler extends DefaultHandler {
+  
+  public static final String LIST = "list";
+  
+  public static final String VALUE = "value";
+  
 
   private final LinkedList<Tuple<String,Map<String,Object>>> elements;
   
@@ -46,53 +50,76 @@ public class XmlMapHandler extends DefaultHandler {
   }
   
   public Map<String,Object> getMap() {
-    return null;
-  }
-  
-  private Map<String,Object> getCurrentMap(boolean create) {
-    return null;
-  }
-  
-  public List<Map<String,Object>> makeAsList(String element, Map<String,Object> map) {
-    
+    return elements.peekFirst().value();
   }
   
   @Override
   public void startElement(String uri, String localName, String qName, Attributes attributes) {
-    if(elements.isEmpty()) {
-      elements.add(new Tuple<>(qName, new HashMap<>()));
-    }
+    elements.add(new Tuple<>(qName, new HashMap<>()));
     setAttributes(qName, attributes);
   }
   
-  
-  private int reverseIndexOf(String elm) {
-    for(int i = (elements.size() -1); i >= 0; i--) {
-      Tuple<String,Map<String,Object>> t = elements.get(i);
-      if(t.key().equals(elm)) return i;
-    }
-    return -1;
+  private Tuple<String,Map<String,Object>> reverseFind(String elm) {
+    return reverseFind(t->t.key().equals(elm));
   }
   
-  private void setAttributes(String elm, Attributes attrs) {
-    int ixe = reverseIndexOf(elm);
-    if(ixe < 0) {
-      
+  private Tuple<String,Map<String,Object>> reverseFind(Predicate<Tuple<String,Map<String,Object>>> prd) {
+    for(int i = (elements.size() -1); i >= 0; i--) {
+      Tuple<String,Map<String,Object>> t = elements.get(i);
+      if(prd.test(t)) {
+        return t;
+      }
     }
-    Tuple<String,Map<String,Object>> t = elements.get(ixe);
+    return null;
+  }
+  
+  private void setAttributes(String name, Attributes attrs) {
+    Tuple<String,Map<String,Object>> elm = reverseFind(name);
+    if(elm == null) {
+      throw new IllegalStateException(String.format("Can't find element (%s) in stack", name));
+    }
+    Map<String,Object> map = elm.value();
     for(int i = 0; i < attrs.getLength(); i++) {
-      t.value().put(attrs.getQName(i), attrs.getValue(i));
+      map.put(attrs.getQName(i), attrs.getValue(i));
     }
   }
   
   @Override
   public void characters(char[] ch, int start, int length) {
     String value = new String(ch, start, length);
+    if(value.trim().isEmpty()) return;
+    Tuple<String,Map<String,Object>> parent = elements.peekLast();
+    parent.value().put(VALUE, value);
   }
   
   @Override
   public void endElement(String uri, String localName, String qName) {
-    elements.pollLast();
+    if(elements.size() < 2) return;
+    Tuple<String,Map<String,Object>> elm = elements.pollLast();
+    Tuple<String,Map<String,Object>> prev = elements.peekLast();
+    Object value;
+    //if is a key-value element
+    if(elm.value().size() == 1 && elm.value().containsKey(VALUE)) {
+      value = elm.value().get(VALUE);
+    }
+    else {
+      value = elm.value();
+    }
+    //duplicates are a list
+    if(prev.value().containsKey(elm.key())) {
+      Object pvalue = prev.value().get(elm.key());
+      List lst;
+      if(List.class.isAssignableFrom(pvalue.getClass())) {
+        lst = (List) pvalue;
+      }
+      else {
+        lst = new LinkedList();
+        lst.add(pvalue);
+      }
+      lst.add(value);
+      value = lst;
+    }
+    prev.value().put(elm.key(), value);
   }
   
 }
--- orb-xml/src/main/java/us/pserver/orb/xml/XmlOrbConfig.java
+++ orb-xml/src/main/java/us/pserver/orb/xml/XmlOrbConfig.java
@@ -22,13 +22,21 @@
 package us.pserver.orb.xml;
 
 import com.google.gson.Gson;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import us.pserver.orb.TypedStrings;
 import us.pserver.orb.OrbConfiguration;
 
@@ -37,16 +45,12 @@ import us.pserver.orb.OrbConfiguration;
  * @author Juno Roesler - juno@pserver.us
  * @version 0.0 - 23/01/2018
  */
-public class JsonOrbConfig implements OrbConfiguration {
+public class XmlOrbConfig implements OrbConfiguration {
   
-  private final Map<String,Object> map;
+  private final InputStream input;
   
-  public JsonOrbConfig(Gson gson, String str) {
-    this.map = gson.fromJson(str, Map.class);
-  }
-  
-  public JsonOrbConfig(Gson gson, Reader reader) {
-    this.map = gson.fromJson(reader, Map.class);
+  public XmlOrbConfig(InputStream in) {
+    this.input = Objects.requireNonNull(in);
   }
   
   @Override
@@ -56,7 +60,15 @@ public class JsonOrbConfig implements OrbConfiguration {
 
   @Override
   public Map<String, Object> getValuesMap() {
-    return map;
+    try {
+      SAXParser ps = SAXParserFactory.newInstance().newSAXParser();
+      XmlMapHandler xmh = new XmlMapHandler();
+      ps.parse(input, xmh);
+      return xmh.getMap();
+    }
+    catch(Exception e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
   }
 
   @Override
@@ -66,33 +78,16 @@ public class JsonOrbConfig implements OrbConfiguration {
   
   
   
-  public static JsonOrbConfig of(String json) {
-    Gson gson = new Gson();
-    return new JsonOrbConfig(gson, json);
+  public static XmlOrbConfig of(String xml) {
+    return of(xml, StandardCharsets.UTF_8);
   }
   
-  public static JsonOrbConfig of(Gson gson, String json) {
-    return new JsonOrbConfig(gson, json);
+  public static XmlOrbConfig of(String xml, Charset cs) {
+    return new XmlOrbConfig(new ByteArrayInputStream(xml.getBytes(cs)));
   }
   
-  public static JsonOrbConfig of(Reader json) {
-    Gson gson = new Gson();
-    return new JsonOrbConfig(gson, json);
-  }
-  
-  public static JsonOrbConfig of(Gson gson, Reader json) {
-    return new JsonOrbConfig(gson, json);
-  }
-  
-  public static JsonOrbConfig of(Path json) throws IOException {
-    Gson gson = new Gson();
-    Reader rdr = Files.newBufferedReader(json);
-    return new JsonOrbConfig(gson, rdr);
-  }
-  
-  public static JsonOrbConfig of(Gson gson, Path json) throws IOException {
-    Reader rdr = Files.newBufferedReader(json);
-    return new JsonOrbConfig(gson, rdr);
+  public static XmlOrbConfig of(Path path) throws IOException {
+    return new XmlOrbConfig(Files.newInputStream(path, StandardOpenOption.READ));
   }
   
 }
