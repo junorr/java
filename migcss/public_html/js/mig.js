60da30b13e882f2632f123187560bbcec8e95f6d
Juno Roesler <juno.rr@gmail.com>
2018-11-20 18:17:09 -0200

    one

--- migcss/public_html/js/mig.js
+++ migcss/public_html/js/mig.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+function MigTooltip(elt) {
+  
+}
 
 
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
